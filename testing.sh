33c3af26486b28b38c93aa6be403c65c1135f01a
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-06-05 15:25:18 +0800

    Fixed #1 update travis.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- testing.sh
+++ testing.sh
@@ -13,9 +13,8 @@ output() {
 git clone https://github.com/bcit-ci/CodeIgniter.git ci
 sed -i.bak -e "s/\.\.\//\.\/ci/g" docker-compose.yml
 output "Start container."
-./run docker-compose -v
-./run docker-compose -p ci build --no-cache
-./run docker-compose -p ci up -d
+docker-compose -p ci build --no-cache
+docker-compose -p ci up -d
 curl -L http://127.0.0.1 > /dev/null
 
 [ $? -eq 0 ] && output "✨ ✨ ✨ OK - Test done."

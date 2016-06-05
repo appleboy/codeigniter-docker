2e2e77dbcc3c129668fb39f8e668029fec2c12f4
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-06-05 15:42:27 +0800

    stop all container after testing.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- testing.sh
+++ testing.sh
@@ -15,7 +15,11 @@ sed -i.bak -e "s/\.\.\//\.\/ci/g" docker-compose.yml
 output "Start container."
 docker-compose -p ci build --no-cache
 docker-compose -p ci up -d
+output "Testing CodeIgniter Web Site."
 curl -L http://127.0.0.1 > /dev/null
 
 [ $? -eq 0 ] && output "✨ ✨ ✨ OK - Test done."
-[ $? -eq 0 ] || output "✨ ✨ ✨ Error - Test fail." 1 && exit 1
+[ $? -eq 0 ] || (output "✨ ✨ ✨ Error - Test fail." 1 && exit 1)
+
+# stop all container
+docker-compose -p ci down

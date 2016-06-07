accf0129a7848f610bd54805c935eab5f3a10c96
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-06-07 23:31:40 +0800

    install ci-phpunit-test
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- testing.sh
+++ testing.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+CURRENT=$(pwd)
+
 output() {
   color="32"
   if [ "$2" = 1 ]; then
@@ -17,6 +19,9 @@ docker-compose -p ci build --no-cache
 docker-compose -p ci up -d
 output "Testing CodeIgniter Web Site."
 curl -L http://127.0.0.1 > /dev/null
+cd ${CURRENT}/ci && composer require phpunit/phpunit@~4.0 --dev
+cd ${CURRENT}/ci && git clone https://github.com/kenjis/ci-phpunit-test.git
+cd ${CURRENT}/ci && php ci-phpunit-test/install.php
 
 [ $? -eq 0 ] && output "✨ ✨ ✨ OK - Test done."
 [ $? -eq 0 ] || (output "✨ ✨ ✨ Error - Test fail." 1 && exit 1)

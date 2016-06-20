a4e745d232b592ae2f922b443f38246983692f15
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-06-20 16:44:37 +0800

    testing in workspace of Docker.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- testing.sh
+++ testing.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 CURRENT=$(pwd)
+PROJECT=$1
 
 output() {
   color="32"
@@ -15,16 +16,20 @@ output() {
 git clone https://github.com/bcit-ci/CodeIgniter.git ci
 sed -i.bak -e "s/\.\.\//\.\/ci/g" docker-compose.yml
 output "Start container."
-docker-compose -p ci build --no-cache
-docker-compose -p ci up -d
+docker-compose -p ${PROJECT} build
+docker-compose -p ${PROJECT} up -d
 output "Testing CodeIgniter Web Site."
 curl -L http://127.0.0.1 > /dev/null
-cd ${CURRENT}/ci && composer require phpunit/phpunit:~4.0 --dev
-cd ${CURRENT}/ci && composer require kenjis/ci-phpunit-test --dev --prefer-dist
-cd ${CURRENT}/ci && php vendor/kenjis/ci-phpunit-test/install.php
-cd ${CURRENT}/ci && git checkout remotes/origin/3.0-stable
+output "Switch codeigniter stable branch."
+docker-compose -p ${PROJECT} run workspace git checkout remotes/origin/3.0-stable
+output "Install phpunit command."
+docker-compose -p ${PROJECT} run workspace composer require phpunit/phpunit:~4.0 --dev --prefer-dist
+output "Download ci-phpunit-test tool."
+docker-compose -p ${PROJECT} run workspace composer require kenjis/ci-phpunit-test --dev --prefer-dist
+output "Install ci-phpunit-test folder."
+docker-compose -p ${PROJECT} run workspace php vendor/kenjis/ci-phpunit-test/install.php
 output "Testing CodeIgniter php unit test."
-cd ${CURRENT}/ci/application/tests && ../../vendor/bin/phpunit
+docker-compose -p ${PROJECT} run workspace sh -c "cd application/tests && /var/www/codeigniter/vendor/bin/phpunit"
 
 [ $? -eq 0 ] && output "✨ ✨ ✨ OK - Test done."
 [ $? -eq 0 ] || (output "✨ ✨ ✨ Error - Test fail." 1 && exit 1)
